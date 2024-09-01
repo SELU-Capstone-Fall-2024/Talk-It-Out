76c9a2361c6a8f207da64d708c83a2a1999dcc61
woycy <thatguyuno55@gmail.com>
2024-09-01 16:36:08 -0500

    swagger setup

--- TalkItOut/TalkItOut/Startup.cs
+++ TalkItOut/TalkItOut/Startup.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 
 namespace TalkItOut;
@@ -20,6 +22,15 @@ public class Startup
     {
         // Add services to the container
         services.AddControllers();
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "TalkItOutAPI", Version = "v1" });
+
+            // Add support for XML comments
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            options.IncludeXmlComments(xmlPath);
+        });
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -33,6 +44,13 @@ public class Startup
             app.UseExceptionHandler("/Home/Error");
             app.UseHsts();
         }
+        
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "TalkItOutAPI v1");
+            options.RoutePrefix = string.Empty; // Set the Swagger UI at the root URL
+        });
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
