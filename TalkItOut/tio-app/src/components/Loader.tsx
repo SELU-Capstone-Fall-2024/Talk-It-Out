27ff1f6ced3d862eedc0132e4479d9c058e74b21
allielemmond <allie.lemmond@gmail.com>
2024-09-19 15:01:33 -0500

    loader component, refactored

---
+++ TalkItOut/tio-app/src/components/Loader.tsx
@@ -0,0 +1,8 @@
+import { Stack, Spinner } from 'tamagui';
+
+const Loader = () => (
+  <Stack alignItems="center" justifyContent="center" height="100%">
+    <Spinner size="large" />
+  </Stack>
+);
+export default Loader;
--- TalkItOut/tio-app/src/components/Users.tsx
+++ TalkItOut/tio-app/src/components/Users.tsx
@@ -2,29 +2,33 @@ import React from 'react';
 import api from '../api/api';
 import { User } from '../types';
 import { useAsync } from 'react-use';
-
-const getUsers = async (): Promise<User[]> => {
-  const response = await api.get('/users');
-  return response.data;
-};
+import Loader from '../components/Loader';
 
 const Users: React.FC = () => {
-  const { loading, error, value: users } = useAsync(getUsers);
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error loading users</div>;
-  if (users && users.length === 0) return <div>No users found.</div>;
+  const { loading, value: users, error } = useAsync( async () => {
+    try {
+      const response = await api.get('/users');
+      return response.data; // Axios places data here by default
+    } catch (error) {
+      console.error('Failed to fetch users:', error);
+      return [];
+    }
+  });
 
   return (
     <div>
       <h1>Users</h1>
-      <ul>
-        {users?.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      {loading && <Loader />}
+      {error && <div>Error loading users.</div>}
+      {!loading && !error && users?.length === 0 && <div>No users found.</div>}
+      {!loading && users && (
+        <ul>
+          {users.map((user: User) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-};
-
+}; 
 export default Users;
