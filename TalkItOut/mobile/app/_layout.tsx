5f841c71570f83211bf3e81e3d8caeba4bf34c51
Stephen Legnon <thatguyuno55@gmail.com>
2024-10-04 00:09:23 -0500

    wip

--- TalkItOut/mobile/app/_layout.tsx
+++ TalkItOut/mobile/app/_layout.tsx
@@ -3,7 +3,43 @@ import {Stack} from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="(tabs)" />
     </Stack>
   );
 }
+// import {StatusBar} from 'expo-status-bar';
+// import {useColorScheme, Text} from 'react-native';
+// import {Paragraph, Spacer, TamaguiProvider, Theme, YStack} from 'tamagui';
+// import {useFonts} from 'expo-font';
+// import config from '@/tamagui.config';
+// import {Stack} from 'expo-router';
+// export default function RootLayout() {
+//   const colorScheme = useColorScheme();
+//   const [loaded] = useFonts({
+//     Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+//     InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+//   });
+//   if (!loaded) {
+//     return null;
+//   }
+//   return (
+//     <TamaguiProvider config={config}>
+//       <Theme name={colorScheme === 'dark' ? 'dark' : 'light'}>
+//         <YStack
+//           f={1}
+//           jc="center"
+//           ai="center"
+//           backgroundColor={'$backgroundSoft'}
+//         >
+//           <Paragraph color="$color" jc="center">
+//             {colorScheme}
+//             <Stack>
+//               <Stack.Screen name="(tabs)" />
+//             </Stack>
+//           </Paragraph>
+//           <StatusBar style="auto" />
+//         </YStack>
+//       </Theme>
+//     </TamaguiProvider>
+//   );
+// }
