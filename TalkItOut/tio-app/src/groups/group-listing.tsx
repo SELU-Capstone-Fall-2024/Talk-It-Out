c4679bc71b636eef5899d531be00894d61e789f2
allielemmond <allie.lemmond@gmail.com>
2024-10-12 20:15:55 -0500

    group listing page

--- TalkItOut/tio-app/src/groups/group-listing.tsx
+++ TalkItOut/tio-app/src/groups/group-listing.tsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import api from '../api/api';
-import {Response, GroupGetDto, ClientGetDto} from '../types';
-import {useAsync} from 'react-use';
-import Loader from '../components/loader';
+import React from "react";
+import api from "../api/api";
+import { Response, GroupGetDto, ClientGetDto } from "../types";
+import { useAsync } from "react-use";
+import { Spinner } from "tamagui";
 
 const Groups: React.FC = () => {
-  const {loading, value: groups} = useAsync(async () => {
-    const response = await api.get<Response<GroupGetDto[]>>('/groups');
+  const { loading, value: groups } = useAsync(async () => {
+    const response = await api.get<Response<GroupGetDto[]>>("/groups");
     return response.data;
   });
 
   return (
     <div>
       <h1>Groups</h1>
-      {loading && <Loader />}
+      {loading && <Spinner />}
       {groups?.hasErrors && <div>Error loading groups.</div>}
       {groups && !loading && groups.data?.length === 0 && (
         <div>No groups found.</div>
       )}
       {!loading && groups && (
         <ul>
-        {groups.data?.map((group) => (
-          <li key={group.id}>
-            <strong>Group ID: {group.id}</strong>
-            <ul>
-              {/* Map through clients and display relevant info */}
-              {group.clients?.map((client: ClientGetDto) => (
-                <li key={client.id}>
-                  {client.firstName}
-                  {client.lastname}
-                  {client.dateOfBirth}
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+          {groups.data?.map((group) => (
+            <li key={group.id}>
+              <strong>Group ID: {group.id}</strong>
+              <ul>
+                {/* Map through clients and display relevant info */}
+                {group.clients?.map((client: ClientGetDto) => (
+                  <li key={client.id}>
+                    {client.firstName}
+                    {client.lastName}
+                    {client.dateOfBirth}
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
       )}
     </div>
   );
