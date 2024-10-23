19f9674ea86a5a079cab94a411c3fcb3ea3f1a2b
allielemmond <allie.lemmond@gmail.com>
2024-10-23 01:59:41 -0500

    matching main

--- TalkItOut/tio-app/tamagui.config.ts
+++ TalkItOut/tio-app/tamagui.config.ts
@@ -1,4 +1,4 @@
-import {createTamagui, createTokens} from '@tamagui/core';
+import { createTamagui, createTokens } from "@tamagui/core";
 
 export default createTamagui({
   tokens: createTokens({
@@ -6,46 +6,6 @@ export default createTamagui({
       sm: 38,
       md: 46,
       lg: 60,
-    },
-    space: {
-      sm: 15,
-      md: 20,
-      lg: 25,
-    },
-    radius: {
-      sm: 4,
-      md: 8,
-      lg: 12,
-    },
-    color: {
-      blue: '#AAAAAA',
-    },
-    zIndex: {
-      low: 1,
-    },
-  }),
-  components: {
-    Button: {
-      variants: {
-        primary: {
-          backgroundColor: '$primary',
-          color: 'white',
-          padding: 12,
-          borderRadius: 8,
-        },
-        secondary: {
-          backgroundColor: '$secondary',
-          color: 'white',
-          padding: 12,
-          borderRadius: 8,
-        },
-      },
-    },
-  },
-  themes: {
-    light: {
-      background: '#fff',
-      color: '#000',
     },
     space: {
       sm: 15,
