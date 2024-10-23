03600102ddedc1639e74661a1584e0f07199f2ff
allielemmond <allie.lemmond@gmail.com>
2024-10-23 01:16:44 -0500

    edits

--- TalkItOut/tio-app/src/components/calendar.tsx
+++ TalkItOut/tio-app/src/components/calendar.tsx
@@ -1,21 +1,19 @@
 import { useEffect, useState } from "react";
 import Calendar, { CalendarProps } from "react-calendar";
 import { View, Text } from "tamagui";
-import api from "../api/api"; // Import your API module
-import { Response, SessionGetDto } from "../types"; // Import your types
-import "./MyCalendar.css"; // Add external CSS here
+import api from "../api/api";
+import { Response, SessionGetDto } from "../types";
+import "./MyCalendar.css";
 
 const MyCalendar = () => {
   const [date, setDate] = useState<Date | [Date, Date] | null>(new Date());
   const [sessions, setSessions] = useState<SessionGetDto[]>([]); // Initialize as an empty array
 
-  // Fetch sessions when the component mounts
   useEffect(() => {
     const fetchSessions = async () => {
       try {
         const response = await api.get<Response<SessionGetDto[]>>("/sessions");
-        // Set sessions to an empty array if response.data.data is null
-        setSessions(response.data.data || []); // Use an empty array as fallback
+        setSessions(response.data.data || []);
       } catch (error) {
         console.error("Error fetching sessions:", error);
       }
@@ -26,9 +24,9 @@ const MyCalendar = () => {
 
   const handleDateChange: CalendarProps["onChange"] = (value) => {
     if (Array.isArray(value)) {
-      setDate(value as [Date, Date]); // Value is a range of dates
+      setDate(value as [Date, Date]);
     } else {
-      setDate(value as Date); // Value is a single date
+      setDate(value as Date);
     }
   };
 
@@ -47,10 +45,10 @@ const MyCalendar = () => {
       <Calendar
         onChange={handleDateChange}
         value={date}
-        className="custom-calendar" // Applying a custom class for styling
+        className="custom-calendar"
         tileClassName={({ date }) =>
           isDateInSession(date) ? "highlighted-session" : ""
-        } // Highlighting session dates
+        }
       />
       <View>
         <Text>Sessions:</Text>
