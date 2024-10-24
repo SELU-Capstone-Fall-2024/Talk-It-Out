417825f2b27e81849938a0a41f64642bfb2625af
Stephen Legnon <thatguyuno55@gmail.com>
2024-10-24 01:15:13 -0500

    respace clients

--- TalkItOut/mobile/app/(tabs)/index.tsx
+++ TalkItOut/mobile/app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import {useState, useEffect} from 'react';
 import api from '../api/api';
 import {ClientGetDto, Response} from '../types';
 import {ClientModal} from './modal';
+import {YStack} from 'tamagui';
 
 export default function Index() {
   const [isLoading, setLoading] = useState(true);
@@ -27,24 +28,24 @@ export default function Index() {
     <View
       style={{
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
       }}
     >
-      <Text>Clients</Text>
-      <Text>
+      <YStack style={{flex: 1, flexDirection: 'column'}}>
+        <Text style={{fontSize: 30}}>Clients</Text>
         {clients && (
           <>
             {clients.map((client) => {
               return (
-                <>
+                <Text>
                   <ClientModal client={client} key={client.id} />
-                </>
+                </Text>
               );
             })}
           </>
         )}
-      </Text>
+      </YStack>
     </View>
   );
 }
