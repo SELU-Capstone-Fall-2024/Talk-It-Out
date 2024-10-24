52e57360d6a73815fab3e63ca75e85fc9213ac42
Stephen Legnon <thatguyuno55@gmail.com>
2024-10-24 10:11:46 -0500

    wip

---
+++ TalkItOut/mobile/app/counter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const NewScreen = () => {
+  return (
+    <View>
+      <Text>This is the New Screen!</Text>
+    </View>
+  );
+};
+
+export default NewScreen;
