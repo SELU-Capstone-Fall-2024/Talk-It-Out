4c0f61bbb63c7789f9062867ed7cd978e807993f
Joseph Overstreet <joseph.overstreet@selu.edu>
2024-11-02 18:36:45 -0500

    Tamagui config test

--- TalkItOut/tio-app/src/components/home.tsx
+++ TalkItOut/tio-app/src/components/home.tsx
@@ -1,6 +1,23 @@
-import { H1 } from "tamagui";
+import {YStack, H1, ButtonFrame} from "tamagui";
+import { InputText } from "../../tamagui.config";
 
 const Home: React.FC = () => {
-  return <H1 color="#e6f2ff">Welcome</H1>;
+  return (
+
+    <YStack
+    flex={1}
+    justifyContent="center"
+    padding={20}
+    background="$darkBackground"
+    minHeight="100vh"
+    width="100vw"
+    >
+      <H1 >Upcoming Sessions</H1>
+      <ButtonFrame backgroundColor="#aaa">
+        Session 1
+      </ButtonFrame>
+    </YStack>
+
+  );
 };
 export default Home;
