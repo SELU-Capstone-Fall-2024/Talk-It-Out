384a039686188a02854dad6d2d6ad3ea912bcfef
allielemmond <allie.lemmond@gmail.com>
2024-12-02 20:08:27 -0600

    fixed delete client

--- TalkItOut/TalkItOut/Entities/Client.cs
+++ TalkItOut/TalkItOut/Entities/Client.cs
@@ -14,6 +14,7 @@ public class Client
     public int? GroupId { get; set; }
     public Group Group { get; set; }
     public List<Goal> Goals { get; set; }
+    public List<Session> Sessions { get; set; }
 }
 
 public class ClientGetDto
@@ -51,5 +52,15 @@ public class ClientConfiguration : IEntityTypeConfiguration<Client>
     public void Configure(EntityTypeBuilder<Client> builder)
     {
         builder.ToTable("Clients", "domain");
+        
+        builder.HasMany(c => c.Goals)
+            .WithOne(g => g.Client)
+            .HasForeignKey(g => g.ClientId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        builder.HasMany(c => c.Sessions)
+            .WithOne(s => s.Client)
+            .HasForeignKey(s => s.ClientId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
