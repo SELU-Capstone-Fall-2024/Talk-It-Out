a9292acfd0f27825531b4a2876883b95e135e42b
Stephen Legnon <thatguyuno55@gmail.com>
2024-12-04 18:35:50 -0600

    fix open

--- TalkItOut/tio-app/src/components/delete-modal.tsx
+++ TalkItOut/tio-app/src/components/delete-modal.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Dialog, YStack, XStack, Button, SizableText, Text } from "tamagui";
+import React from 'react';
+import {Dialog, YStack, XStack, Button, SizableText, Text} from 'tamagui';
 
 interface DeleteModalProps {
   isOpen: boolean;
@@ -12,63 +12,68 @@ const DeleteModal: React.FC<DeleteModalProps> = ({
   onClose,
   onConfirm,
 }) => (
-  <Dialog open={isOpen} onOpenChange={onClose}>
-    <Dialog.Overlay
-      animation="quick"
-      enterStyle={{ opacity: 0 }}
-      exitStyle={{ opacity: 0 }}
-      opacity={0.8}
-      backgroundColor="rgba(0, 0, 0, 0.5)"
-    />
-    <Dialog.Content
-      elevate
-      bordered
-      borderRadius={16}
-      backgroundColor="white"
-      padding={20}
-      animation={[
-        "quick",
-        {
-          opacity: {
-            overshootClamping: true,
-          },
-          scale: {
-            overshootClamping: true,
-          },
-        },
-      ]}
-    >
-      <YStack alignItems="center" gap={15}>
-        <SizableText size={20} fontWeight="bold" color="black">
-          Confirm Deletion
-        </SizableText>
-        <Text fontSize={16} color="gray">
-          Are you sure you want to delete this? This action cannot be undone.
-        </Text>
-        <XStack
-          gap={10}
-          alignItems="center"
-          justifyContent="space-between"
-          width={"100%"}
+  <>
+    {isOpen && (
+      <Dialog onOpenChange={onClose}>
+        <Dialog.Overlay
+          animation="quick"
+          enterStyle={{opacity: 0}}
+          exitStyle={{opacity: 0}}
+          opacity={0.8}
+          backgroundColor="rgba(0, 0, 0, 0.5)"
+        />
+        <Dialog.Content
+          elevate
+          bordered
+          borderRadius={16}
+          backgroundColor="white"
+          padding={20}
+          animation={[
+            'quick',
+            {
+              opacity: {
+                overshootClamping: true,
+              },
+              scale: {
+                overshootClamping: true,
+              },
+            },
+          ]}
         >
-          <YStack>
-            <Button size={40} onPress={onClose}>
-              <Text>Cancel</Text>
-            </Button>
-          </YStack>
-          <YStack>
-            <Button
-              size={40}
-              style={{ background: "#b32d00" }}
-              onPress={onConfirm}
+          <YStack alignItems="center" gap={15}>
+            <SizableText size={20} fontWeight="bold" color="black">
+              Confirm Deletion
+            </SizableText>
+            <Text fontSize={16} color="gray">
+              Are you sure you want to delete this? This action cannot be
+              undone.
+            </Text>
+            <XStack
+              gap={10}
+              alignItems="center"
+              justifyContent="space-between"
+              width={'100%'}
             >
-              <Text color={"white"}>Delete</Text>
-            </Button>
+              <YStack>
+                <Button size={40} onPress={onClose}>
+                  <Text>Cancel</Text>
+                </Button>
+              </YStack>
+              <YStack>
+                <Button
+                  size={40}
+                  style={{background: '#b32d00'}}
+                  onPress={onConfirm}
+                >
+                  <Text color={'white'}>Delete</Text>
+                </Button>
+              </YStack>
+            </XStack>
           </YStack>
-        </XStack>
-      </YStack>
-    </Dialog.Content>
-  </Dialog>
+        </Dialog.Content>
+      </Dialog>
+    )}
+  </>
 );
 
 export default DeleteModal;
